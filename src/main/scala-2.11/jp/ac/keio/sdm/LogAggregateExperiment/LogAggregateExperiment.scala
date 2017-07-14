a6c6daeca179c23b822184111c8883243f7e93ef
Ryuichi Saito <ryuichi.saito@keio.jp>
2017-07-14 17:46:22 +0900

    Implement groupBy query.

--- src/main/scala-2.11/jp/ac/keio/sdm/LogAggregateExperiment/LogAggregateExperiment.scala
+++ src/main/scala-2.11/jp/ac/keio/sdm/LogAggregateExperiment/LogAggregateExperiment.scala
@@ -1,19 +1,10 @@
 package jp.ac.keio.sdm.LogAggregateExperiment
 
-import io.netty.handler.codec.string
-import org.apache.spark.SparkConf
-import org.apache.spark.SparkContext
+import kafka.serializer.StringDecoder
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.streaming.kafka.KafkaUtils
 import org.apache.spark.streaming.{Seconds, StreamingContext}
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.types._
-import org.apache.spark.sql.functions.from_json
-import kafka.serializer.StringDecoder
-
-import kafka.serializer.StringDecoder
-
-import org.apache.spark.streaming._
-import org.apache.spark.streaming.kafka._
+import org.apache.spark.{SparkConf, SparkContext}
 
 /**
   * Created by Ryuichi on 6/2/2017 AD.
@@ -23,6 +14,25 @@ object LogAggregateExperiment {
   final val threadCount = 2
   final val applicationName = "LogAggregateExperiment"
   final val batchDuration = 2
+  final val upperLimit = 10000
+  final val log_level = "log_level"
+  final val multi_thread_id = "multi_thread_id"
+  final val message = "message"
+  final val stack_trace_01 = "stack_trace_01"
+  final val stack_trace_02 = "stack_trace_02"
+  final val stack_trace_03 = "stack_trace_03"
+  final val stack_trace_04 = "stack_trace_04"
+  final val stack_trace_05 = "stack_trace_05"
+  final val stack_trace_06 = "stack_trace_06"
+  final val stack_trace_07 = "stack_trace_07"
+  final val stack_trace_08 = "stack_trace_08"
+  final val stack_trace_09 = "stack_trace_09"
+  final val stack_trace_10 = "stack_trace_10"
+  final val stack_trace_11 = "stack_trace_11"
+  final val stack_trace_12 = "stack_trace_12"
+  final val stack_trace_13 = "stack_trace_13"
+  final val stack_trace_14 = "stack_trace_14"
+
 
   def main(args: Array[String]) {
 
@@ -48,8 +58,6 @@ object LogAggregateExperiment {
       /*.config("spark.some.config.option", "some-value")*/
       .getOrCreate()
 
-    import spark.implicits._
-
     /*ssc.checkpoint("checkpoint")*/
 
     val topicsSet = topics.split(",").toSet
@@ -65,7 +73,45 @@ object LogAggregateExperiment {
        val data = spark.read.option("wholeFile", true).json(jsonRDD)
        if (data.count() > 0) {
          data.printSchema()
-         data.groupBy("stack_trace_01").count().show(100000)
+         // data.groupBy("stack_trace_01").count().show(upperLimit)
+         import org.apache.spark.sql.functions._
+         val resultSetByGroupBy = data.groupBy(
+           log_level
+           ,multi_thread_id
+           ,stack_trace_01
+           ,stack_trace_02
+           ,stack_trace_03
+           ,stack_trace_04
+           ,stack_trace_05
+           ,stack_trace_06
+           ,stack_trace_07
+           ,stack_trace_08
+           ,stack_trace_09
+           ,stack_trace_10
+           ,stack_trace_11
+           ,stack_trace_12
+           ,stack_trace_13
+           ,stack_trace_14).agg(min(message))
+         resultSetByGroupBy.show(upperLimit) // OK
+         data.show(upperLimit) // OK
+
+
+         //data.createOrReplaceTempView("data")
+         //resultSetByGroupBy.createOrReplaceTempView("resultSetByGroupBy")
+
+         //val df = data.join(resultSetByGroupBy, data.col("stack_trace_01") === resultSetByGroupBy.col("stack_trace_01"), "inner")
+         //val df = spark.sql("select * from data right join resultSetByGroupBy on data.stack_trace_01 == resultSetByGroupBy.stack_trace_01")
+
+         // val df = resultSetByGroupBy.join(data, resultSetByGroupBy.col("stack_trace_01") === data.col("stack_trace_01"))
+         //df.show(upperLimit)
+//         resultSetByGroupBy.join(data)
+//         resultSetByGroupBy.foreach(f => {
+//           // data.show(upperLimit)
+//           // '===' returns a column which contains the result of the comparisons of the elements of two columns
+//           // val resultSetByFilter = data.filter($"stack_trace_01" === f.getAs("stack_trace_01"))
+//           data.filter($"stack_trace_01" === f.getAs("stack_trace_01"))
+//           // resultSetByFilter.show()
+//         })
        }
        // data.createOrReplaceTempView("log")
        // val df = sqlContext.sql("SELECT * FROM log"
