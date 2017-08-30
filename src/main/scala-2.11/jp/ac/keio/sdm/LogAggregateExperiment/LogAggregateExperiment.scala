dc89c6d1fa5d1d3a0d579e67357e93777e9dedc5
Ryuichi Saito <ryuichi.saito@keio.jp>
2017-08-30 11:48:37 +0900

    Refactor code.

--- src/main/scala-2.11/jp/ac/keio/sdm/LogAggregateExperiment/LogAggregateExperiment.scala
+++ src/main/scala-2.11/jp/ac/keio/sdm/LogAggregateExperiment/LogAggregateExperiment.scala
@@ -1,37 +1,38 @@
 package jp.ac.keio.sdm.LogAggregateExperiment
 
 import kafka.serializer.StringDecoder
+import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.streaming.kafka.KafkaUtils
 import org.apache.spark.streaming.{Seconds, StreamingContext}
-import org.apache.spark.{SparkConf, SparkContext}
 
 /**
   * Created by Ryuichi on 6/2/2017 AD.
   */
 object LogAggregateExperiment {
 
-  final val threadCount = 2
-  final val applicationName = "LogAggregateExperiment"
-  final val batchDuration = 2
-  final val upperLimit = 10000
-  final val log_level = "log_level"
-  final val multi_thread_id = "multi_thread_id"
-  final val message = "message"
-  final val stack_trace_01 = "stack_trace_01"
-  final val stack_trace_02 = "stack_trace_02"
-  final val stack_trace_03 = "stack_trace_03"
-  final val stack_trace_04 = "stack_trace_04"
-  final val stack_trace_05 = "stack_trace_05"
-  final val stack_trace_06 = "stack_trace_06"
-  final val stack_trace_07 = "stack_trace_07"
-  final val stack_trace_08 = "stack_trace_08"
-  final val stack_trace_09 = "stack_trace_09"
-  final val stack_trace_10 = "stack_trace_10"
-  final val stack_trace_11 = "stack_trace_11"
-  final val stack_trace_12 = "stack_trace_12"
-  final val stack_trace_13 = "stack_trace_13"
-  final val stack_trace_14 = "stack_trace_14"
+  val ThreadCount = 2
+  val SparkUrl = "local[" + ThreadCount + "]"
+  val ApplicationName = "LogAggregateExperiment"
+  val BatchDuration = 2
+  val UpperLimit = 10000
+  val LogLevel = "log_level"
+  val MultiThreadId = "multi_thread_id"
+  val Message = "message"
+  val StackTrace01 = "stack_trace_01"
+  val StackTrace02 = "stack_trace_02"
+  val StackTrace03 = "stack_trace_03"
+  val StackTrace04 = "stack_trace_04"
+  val StackTrace05 = "stack_trace_05"
+  val StackTrace06 = "stack_trace_06"
+  val StackTrace07 = "stack_trace_07"
+  val StackTrace08 = "stack_trace_08"
+  val StackTrace09 = "stack_trace_09"
+  val StackTrace10 = "stack_trace_10"
+  val StackTrace11 = "stack_trace_11"
+  val StackTrace12 = "stack_trace_12"
+  val StackTrace13 = "stack_trace_13"
+  val StackTrace14 = "stack_trace_14"
 
 
   def main(args: Array[String]) {
@@ -48,22 +49,19 @@ object LogAggregateExperiment {
     }
 
     val Array(brokers, topics) = args
-    val sparkUrl = "local[" + threadCount + "]"
-    val sparkConf = new SparkConf().setMaster(sparkUrl).setAppName(applicationName)
-    val sc = new SparkContext(sparkConf)
-    val ssc = new StreamingContext(sc, Seconds(batchDuration))
+    val sparkConf = new SparkConf().setMaster(SparkUrl).setAppName(ApplicationName)
+    val ssc = new StreamingContext(sparkConf, Seconds(BatchDuration))
+    // val sc = new SparkContext(sparkConf)
+    // val ssc = new StreamingContext(sc, Seconds(BatchDuration))
     val spark = SparkSession
       .builder()
-      .appName(applicationName)
-      /*.config("spark.some.config.option", "some-value")*/
+      .appName(ApplicationName)
+      // .config("spark.some.config.option", "some-value")
       .getOrCreate()
-
-    /*ssc.checkpoint("checkpoint")*/
 
     val topicsSet = topics.split(",").toSet
     val kafkaParams = Map[String, String]("metadata.broker.list" -> brokers)
-    val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
-      ssc, kafkaParams, topicsSet)
+    val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topicsSet)
     // "map(_._2)" equals "map(x => (x._2))"
     // x._2 returns the second element of a tuple
     val lines = messages.map(_._2)
@@ -75,82 +73,28 @@ object LogAggregateExperiment {
          data.printSchema()
          // data.groupBy("stack_trace_01").count().show(upperLimit)
          import org.apache.spark.sql.functions._
-         val resultSetByGroupBy = data.groupBy(
-           log_level
-           ,multi_thread_id
-           ,stack_trace_01
-           ,stack_trace_02
-           ,stack_trace_03
-           ,stack_trace_04
-           ,stack_trace_05
-           ,stack_trace_06
-           ,stack_trace_07
-           ,stack_trace_08
-           ,stack_trace_09
-           ,stack_trace_10
-           ,stack_trace_11
-           ,stack_trace_12
-           ,stack_trace_13
-           ,stack_trace_14).agg(min(message))
-         resultSetByGroupBy.show(upperLimit) // OK
-         data.show(upperLimit) // OK
-
-
-         //data.createOrReplaceTempView("data")
-         //resultSetByGroupBy.createOrReplaceTempView("resultSetByGroupBy")
-
-         //val df = data.join(resultSetByGroupBy, data.col("stack_trace_01") === resultSetByGroupBy.col("stack_trace_01"), "inner")
-         //val df = spark.sql("select * from data right join resultSetByGroupBy on data.stack_trace_01 == resultSetByGroupBy.stack_trace_01")
-
-         // val df = resultSetByGroupBy.join(data, resultSetByGroupBy.col("stack_trace_01") === data.col("stack_trace_01"))
-         //df.show(upperLimit)
-//         resultSetByGroupBy.join(data)
-//         resultSetByGroupBy.foreach(f => {
-//           // data.show(upperLimit)
-//           // '===' returns a column which contains the result of the comparisons of the elements of two columns
-//           // val resultSetByFilter = data.filter($"stack_trace_01" === f.getAs("stack_trace_01"))
-//           data.filter($"stack_trace_01" === f.getAs("stack_trace_01"))
-//           // resultSetByFilter.show()
-//         })
+         val resultSetByGroupBy = data.groupBy(LogLevel
+           ,MultiThreadId
+           ,StackTrace01
+           ,StackTrace02
+           ,StackTrace03
+           ,StackTrace04
+           ,StackTrace05
+           ,StackTrace06
+           ,StackTrace07
+           ,StackTrace08
+           ,StackTrace09
+           ,StackTrace10
+           ,StackTrace11
+           ,StackTrace12
+           ,StackTrace13
+           ,StackTrace14).agg(min(Message))
+         resultSetByGroupBy.show(UpperLimit)
+         data.show(UpperLimit)
        }
-       // data.createOrReplaceTempView("log")
-       // val df = sqlContext.sql("SELECT * FROM log"
-
-       /*resultSet.map(t => "Value: " + t(0)).collect().foreach(println)*/
     })
-
-    /*val words = lines.flatMap(_.split(" "))
-    val wordCount = words.map(x => (x, 1L)).reduceByKeyAndWindow(_ + _, _ - _, Minutes(10), Seconds(2), 2)
-    wordCount.print()*/
-
+    
     ssc.start()
     ssc.awaitTermination()
-
-    /*System.setProperty("hadoop.home.dir", "/usr/local/share/hadoop/hadoop-2.7.3")
-    val spark = SparkSession
-      .builder()
-      .master("local")
-      .appName("Log Aggregate Experiment")
-      //      .config("spark.some.config.option", "some-value")
-      .getOrCreate()
-
-    val df = spark
-      .readStream
-      .format("kafka")
-      .option("kafka.bootstrap.servers", "localhost:9092")
-      .option("subscribe", "YUI")
-      .load()
-    val schema: StructType = StructType(Seq(
-      StructField("k",  StringType, true), StructField("v",  StringType, true)
-    ))
-    import spark.implicits._
-    df.printSchema()
-    df.select(from_json($"value".cast(StringType), schema))
-    df.createOrReplaceTempView("LOG")
-    val sqlDf = spark.sql("SELECT * FROM LOG")
-    sqlDf
-      .writeStream
-      .format("console")
-      .start()*/
   }
 }
